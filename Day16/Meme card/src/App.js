45629e51f275199fbfa91c5ed680760826beed30
Suraj Soni <77867885+Suraj-Spasm@users.noreply.github.com>
2021-07-12 12:51:30 +0530

    updated

---
+++ Day16/Meme card/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+    return(
+        <div className="memeCard">
+        <div className="cardImg">
+          <img
+            src="https://s3.amazonaws.com/discourseproduction/original/2X/b/b11120ca4110266072bbb988f01f5c054ca0285d.jpeg"
+            alt="pic"
+          ></img>
+        </div>
+        <h1>MEME CARD</h1>
+        <div className="cardDescription">
+          <p> Life full of struggles </p>
+        </div>
+      </div>
+    );
+}
+
+export default App;
